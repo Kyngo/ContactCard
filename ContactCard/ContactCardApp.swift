77c889952bd79190d2941477ce27934dd77cf863
Arnau Martin <arnau@offing.es>
2022-03-02 09:59:13 +0100

    Initial Commit

---
+++ ContactCard/ContactCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactCardApp.swift
+//  ContactCard
+//
+//  Created by Arnau Martín on 2/3/22.
+//
+
+import SwiftUI
+
+@main
+struct ContactCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
