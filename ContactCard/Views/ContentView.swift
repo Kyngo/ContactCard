d2fa60bfd62e101ceec1182477d4678461afe4e4
Arnau Martin <arnau@offing.es>
2022-03-11 08:45:51 +0100

    ui redesign (WIP)

--- ContactCard/Views/ContentView.swift
+++ ContactCard/Views/ContentView.swift
@@ -37,27 +37,47 @@ struct ContentView: View {
     }
     
     var body: some View {
-        NavigationView {
-            List {
-                renderList()
+        List {
+            ZStack(alignment: .bottom){
+                Image("MyPhoto")
+                    .resizable()
+                    .scaledToFit()
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text("Arnau Martín")
+                            .font(.headline)
+                            .fontWeight(.black)
+                            .colorInvert()
+                            .dynamicTypeSize(/*@START_MENU_TOKEN@*/.xxxLarge/*@END_MENU_TOKEN@*/)
+                        Text("Full Stack Developer")
+                            .font(.subheadline)
+                            .colorInvert()
+                            .dynamicTypeSize(.small)
+                    }
+                    Spacer()
+                }
+                .padding()
             }
-            .navigationTitle("Arnau Martín")
-            .toolbar {
-                ToolbarItemGroup(placement: .bottomBar) {
-                    Button("QR vCard") {
-                        renderQR.toggle()
-                    }
-                    .sheet(isPresented: $renderQR) {
-                        QRView(isPresented: $renderQR)
-                    }
-                    Button("Exit") {
-                        exit(0)
-                    }
+            .padding(.horizontal, -16)
+            renderList()
+        }
+        .padding(.top, -60.0)
+        .listStyle(PlainListStyle())
+        .navigationBarTitle("")
+        .navigationBarHidden(true)
+        .toolbar {
+            ToolbarItemGroup(placement: .bottomBar) {
+                Button("QR vCard") {
+                    renderQR.toggle()
+                }
+                .sheet(isPresented: $renderQR) {
+                    QRView(isPresented: $renderQR)
                 }
             }
         }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
